33b65242971f530f3f7dfc37f1af1271821b8b66
Mr2803 <rispoli.mar2803@gmail.com>
2019-11-11 16:34:11 +0100

    now you can send the message with the enter button, only if you are inside the input to send the message

--- js/script.js
+++ js/script.js
@@ -11,32 +11,45 @@ $(document).ready(function () {
 
 
 
-   $(".fas").click(function () {
+   $(".fas").click(send)
 
-      //ci salviamo il valore dell'input inserito dall'utente
-      var messaggio = $(".message").val();
-      // console.log(messaggio);
+   $(".message").keydown(function (j) {
+      console.log(j.keyCode)
+      if (j.keyCode == "13") {
+         send();
+      }
 
-      // $("#container").append(
-      //         "<div class='msgsent'>" + messaggio +"</div>"
-      // );
-
-      // cloniamo (facciamo una copia) del div con classe "msgsent" che sta dentro
-      // un div con ID "template"
-      var elementmsg = $("#template .msgsent").clone();
-
-      console.log(elementmsg);
-
-      // modifica questa copia di "msgsent" aggiungendogli il testo del messaggio
-      elementmsg.find(".testo").text(messaggio);
-
-      // appendiamo una copia con testo valorizzato del div "msgsent"
-      $(".my_chat").append(elementmsg);
-
-      // ripuliamo il contenuto dell'input, per UX
-      $(".message").val("");
+   })  
 
 
-   })
+      //salvata la mia funzione per inviare i messaggi come esterna in modo da poter dare al tasto invio lo stesso valore
+      function send(){
+         //ci salviamo il valore dell'input inserito dall'utente
+         var messaggio = $(".message").val();
+         // console.log(messaggio);
+
+         // $("#container").append(
+         //         "<div class='msgsent'>" + messaggio +"</div>"
+         // );
+
+         // cloniamo (facciamo una copia) del div con classe "msgsent" che sta dentro
+         // un div con ID "template"
+         var elementmsg = $("#template .msgsent").clone();
+
+         console.log(elementmsg);
+
+         // modifica questa copia di "msgsent" aggiungendogli il testo del messaggio
+         elementmsg.find(".testo").text(messaggio);
+
+         // appendiamo una copia con testo valorizzato del div "msgsent"
+         $(".my_chat").append(elementmsg);
+
+         // ripuliamo il contenuto dell'input, per UX
+         $(".message").val("");
+
+
+      }
+      
+     
 
 })
